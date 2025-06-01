18e29aedb39724ab2aa5ab1a5b94115ceafcabb6
JorgeZamudio90 <alexcahuil90@gmail.com>
2025-05-31 23:30:16 -0600

    Lock

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'alexc/proyecto-agustin',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'fa9de7573e5c4eff4580d8ef5072d274f6371f83',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'alexc/proyecto-agustin' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'fa9de7573e5c4eff4580d8ef5072d274f6371f83',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
